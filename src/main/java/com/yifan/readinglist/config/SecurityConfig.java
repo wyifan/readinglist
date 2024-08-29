9faa6e47525736a64f54e89d0712125e1f07e6e3
shawn <wyifan@vip.qq.com>
2024-08-29 23:48:47 +0800

    add spring boot securityconfig and pages.

---
+++ src/main/java/com/yifan/readinglist/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.yifan.readinglist.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * package_name: com.yifan.readinglist.config
+ * author: wyifa
+ * date: 2024/8/29
+ * description:
+ * WebSecurityConfigurerAdapter is deprecated.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(registry -> {
+                    registry.requestMatchers("/home").permitAll();
+                    registry.requestMatchers("/admin/**").hasRole("ADMIN");
+                    registry.requestMatchers("/user/**").hasRole("USER");
+                    registry.anyRequest().authenticated();
+                })
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder().username("admin").password("$2a$12$S/jR8/4ECUeT0T6hTjHoJe2ml3U78ZBq3/H8D8jCaEqXcakA6p9aK").roles("ADMIN", "USER").build();
+        UserDetails user = User.builder().username("user").password("$2a$12$Wp.hnX80RlZxJczg2v4FLOktt.N1S.8CnKKHuZfKNG4ua4qDN71Ku").roles("USER").build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // https://bcrypt-generator.com/
+        return new BCryptPasswordEncoder();
+    }
+}

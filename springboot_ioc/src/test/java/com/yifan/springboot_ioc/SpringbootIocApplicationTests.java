709de5c8694ba76c2cdcef8911e93ba756232479
shawn <wyifan@vip.qq.com>
2024-09-11 20:12:51 +0800

    add a new module to test spring boot IOC

---
+++ springboot_ioc/src/test/java/com/yifan/springboot_ioc/SpringbootIocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yifan.springboot_ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
